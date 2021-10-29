5af1f876263f0960c63aa6a1b0c3c0fd151a98e1
Sebastian Ament <sebastianeament@gmail.com>
2021-10-29 17:17:38 -0400

    added compatability and TagBot

--- src/updatableQR.jl
+++ src/updatableQR.jl
@@ -195,10 +195,11 @@ function ensure_space_to_append_column!(rotations::AbstractVector{<:Givens},
 end
 
 """
+```
 remove_column!(F::UpdatableGivensQR, k::Int = size(F, 2))
-
-Updates the existing QR factorization F of a matrix to the factorization of
-the same matrix after its kth column has been deleted.
+```
+Updates the existing QR factorization `F` of a matrix to the factorization of
+the same matrix after its kᵗʰ column has been deleted.
 Computational complexity: O(m^2) where size(F) = (n, m).
 """
 function remove_column!(F::UpdatableGivensQR, k::Int = size(F, 2))
