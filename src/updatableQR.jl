38a9cf7949c2b4da202dcc6ae21b1e5baa334722
Sebastian Ament <sebastianeament@gmail.com>
2021-11-22 14:58:21 -0500

    edited docs

--- src/updatableQR.jl
+++ src/updatableQR.jl
@@ -200,7 +200,7 @@ remove_column!(F::UpdatableGivensQR, k::Int = size(F, 2))
 ```
 Updates the existing QR factorization `F` of a matrix to the factorization of
 the same matrix after its kᵗʰ column has been deleted.
-Computational complexity: O(m^2) where size(F) = (n, m).
+Computational complexity: O(m²) where size(F) = (n, m).
 """
 function remove_column!(F::UpdatableGivensQR, k::Int = size(F, 2))
     i = remove_permutation!(F, k)
